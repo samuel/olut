8b642fd6148c542242ae5cd93ab9ef402683400c
Samuel Stauffer <samuel@descolada.com>
2011-01-28 16:16:26 -0800

    Added setup.py and version

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+import os
+execfile(os.path.join('olut', 'version.py'))
+
+setup(
+    name = 'olut',
+    version = VERSION,
+    description = 'Olut is a packging framework meant for deploying applications',
+    author = 'Samuel Stauffer',
+    author_email = 'samuel@playhaven.com',
+    url = 'https://github.com/playhaven/olut',
+    packages = find_packages(),
+    test_suite = "tests",
+    entry_points = {
+        "console_scripts": [
+            "olut = olut.command:main",
+        ],
+    },
+    classifiers = [
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    install_requires = [],
+)
